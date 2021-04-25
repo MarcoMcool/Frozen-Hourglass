2e0aa807ea5d9466eea39f225aae5200dda6a2c4
smarkovics <smarkovics00@gmail.com>
2021-04-25 20:49:21 +1000

    Added grabbing and teleport

--- Frozen Hourglass/Assets/Oculus/SampleFramework/Core/Locomotion/Scripts/TeleportTransitionBlink.cs
+++ Frozen Hourglass/Assets/Oculus/SampleFramework/Core/Locomotion/Scripts/TeleportTransitionBlink.cs
@@ -17,6 +17,8 @@ using System.Collections;
 /// </summary>
 public class TeleportTransitionBlink : TeleportTransition
 {
+
+	public OVRScreenFade fader;
 	/// <summary>
 	/// How long the transition takes. Usually this is greater than Teleport Delay.
 	/// </summary>
@@ -66,12 +68,12 @@ public class TeleportTransitionBlink : TeleportTransition
 				teleported = true;
 				LocomotionTeleport.DoTeleport();
 			}
-			//float fadeLevel = FadeLevels.Evaluate(elapsedTime / TransitionDuration);
-			//OVRInspector.instance.fader.SetFadeLevel(fadeLevel);
-		}
+            float fadeLevel = FadeLevels.Evaluate(elapsedTime / TransitionDuration);
+            fader.SetExplicitFade(fadeLevel);
+        }
 
-		//OVRInspector.instance.fader.SetFadeLevel(0);
+		fader.SetExplicitFade(0);
 
-		LocomotionTeleport.IsTransitioning = false;
+        LocomotionTeleport.IsTransitioning = false;
 	}
 }
