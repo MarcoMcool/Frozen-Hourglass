60dd6893b146c78c67a2b468482af2750272d505
smarkovics <smarkovics00@gmail.com>
2021-05-02 22:36:53 +1000

    Small fixes to bugs

--- Frozen Hourglass/Assets/Main Project/Scripts/MainMenu.cs
+++ Frozen Hourglass/Assets/Main Project/Scripts/MainMenu.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
    public void PLAY()
     {
         print("play has been pressed.");
+        SceneManager.LoadScene("MainScene");
     }
     
     public void QUIT()
