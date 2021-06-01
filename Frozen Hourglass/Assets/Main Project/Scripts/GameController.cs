112d9dbb12d623be826e38350b5daa9eb0de22b6
MarcoMcool <mattym12@me.com>
2021-06-01 23:06:13 +1000

    Final demo push

--- Frozen Hourglass/Assets/Main Project/Scripts/GameController.cs
+++ Frozen Hourglass/Assets/Main Project/Scripts/GameController.cs
@@ -11,11 +11,13 @@ public class GameController: MonoBehaviour
     public GameObject fullboxq1;
     public GameObject answersq2;
     public GameObject fullboxq2;
+    public GameObject fullboxq3;
     public GameObject text1;
     public GameObject text2;
 
     public bool button1Pressed;
     public bool button2Pressed;
+    public int question =1;
 
     public float timer = 0f;
     //public GameObject button1;
@@ -50,33 +52,52 @@ public class GameController: MonoBehaviour
             button1.NumBoxs = 3;
             button1.SetUp();
         }
-        if (button1Pressed)
+        if (button1Pressed && question ==1)
         {
+            answersq1.SetActive(false);
+            fullboxq1.SetActive(true);
+            timer += Time.deltaTime;
             if (timer >= 5f)
             {
                 text1.SetActive(false);
                 fullboxq1.SetActive(false);
+                answersq1.SetActive(false);
                 answersq2.SetActive(true);
                 text2.SetActive(true);
                 button1Pressed = false;
                 timer = 0f;
                 button1.gameObject.SetActive(false);
                 button2.gameObject.SetActive(true);
-
+                question = 2;
                 button2.SetUp();
             }
-            answersq1.SetActive(false);
-            fullboxq1.SetActive(true);
-            timer += Time.deltaTime;
+            
+            
         }
         if (button2Pressed)
         {
+            answersq2.SetActive(false);
+            fullboxq2.SetActive(true);
             timer += Time.deltaTime;
-            if (timer >= 3f)
+            if (timer >= 5f)
             {
-
-                answersq1.SetActive(false);
-                fullboxq1.SetActive(false);
+                answersq2.SetActive(true);
+                fullboxq2.SetActive(false);
+                button2Pressed = false;
+                timer = 0f;
+            }
+        }
+        if (button1Pressed && question ==2)
+        {
+            answersq2.SetActive(false);
+            fullboxq3.SetActive(true);
+            timer += Time.deltaTime;
+            if (timer >= 5f)
+            {
+                answersq2.SetActive(true);
+                fullboxq3.SetActive(false);
+                button1Pressed = false;
+                timer = 0f;
             }
         }
     }
