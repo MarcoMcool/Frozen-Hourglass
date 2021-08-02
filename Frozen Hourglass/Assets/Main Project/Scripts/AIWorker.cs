f7d92d2b2c179cb42be4be1ab2161a153ed43b64
smarkovics <smarkovics00@gmail.com>
2021-08-02 15:26:14 +1000

    Started Creating IPad and camera

--- Frozen Hourglass/Assets/Main Project/Scripts/AIWorker.cs
+++ Frozen Hourglass/Assets/Main Project/Scripts/AIWorker.cs
@@ -7,12 +7,19 @@ public class AIWorker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("WorkerTimer");
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    IEnumerator WorkerTimer()
+    {
+        yield return new WaitForSeconds(5f);
+        print("waited");
         
     }
 }
