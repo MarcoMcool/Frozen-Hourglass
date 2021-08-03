b4bfeba523386c5cdb3ac228f99da9361758c238
MarkKTH <62837294+MarkKTH@users.noreply.github.com>
2021-08-03 21:46:16 +1000

    Made iPad UI elements

---
+++ Frozen Hourglass/Assets/Main Project/Scripts/IPadMechanics.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IPadMechanics : MonoBehaviour
+{
+
+    // Tasks for creating the IPad are as follows:
+    // Allow the IPad to simulate calling functions - 
+    // A way to get the Ipad to look like they can call a person is to create an interface that shows some kind of button that players can press to change 
+    // what is seen to a screen that can call a person
+
+
+    // Allow the Ipad to simulate photo taking functions
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
