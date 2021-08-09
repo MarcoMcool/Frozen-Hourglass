cd4a3c47238645fe20606c08207cd80794672f9c
smarkovics <smarkovics00@gmail.com>
2021-08-09 17:28:36 +1000

    neaten

--- Frozen Hourglass/Assets/Main Project/Scripts/QAFunctions.cs
+++ Frozen Hourglass/Assets/Main Project/Scripts/QAFunctions.cs
@@ -13,6 +13,7 @@ public class QAFunctions: MonoBehaviour
     public string[] Question2 = new string[6];
 
     // Question 1
+    [Header("Question 1")]
     public string Q1 = "What is the incident that occurred?";
     public string Q1Response1 = "q1 response 1";
     public string Q1Response2 = "q1 response 2";
@@ -21,6 +22,7 @@ public class QAFunctions: MonoBehaviour
     public string Q1InCorrect = "q1 inCorrect";
 
     // Question 2
+    [Header("Question 2")]
     public string Q2 = "Should work in the vicinity be stopped?";
     public string Q2Response1 = "q2 response 1";
     public string Q2Response2 = "q2 response 2";
@@ -29,6 +31,7 @@ public class QAFunctions: MonoBehaviour
     public string Q2InCorrect = "q2 inCorrect";
 
     // Question 3
+    [Header("Question 3")]
     public string Q3 = "Is there any potential risks in the environment?";
     public string Q3Response1 = "q3 response 1";
     public string Q3Response2 = "q3 response 2";
@@ -37,6 +40,7 @@ public class QAFunctions: MonoBehaviour
     public string Q3InCorrect = "q3 inCorrect";
 
     // Question 4
+    [Header("Question 4")]
     public string Q4 = "Which option in the hierarchy of control would best suit solving this risk?";
     public string Q4Response1 = "q4 response 1";
     public string Q4Response2 = "q4 response 2";
@@ -45,6 +49,7 @@ public class QAFunctions: MonoBehaviour
     public string Q4InCorrect = "q4 inCorrect";
 
     // Question 5
+    [Header("Question 5")]
     public string Q5 = "Should the emergency services be called?";
     public string Q5Response1 = "q5 response 1";
     public string Q5Response2 = "q5 response 2";
@@ -53,6 +58,7 @@ public class QAFunctions: MonoBehaviour
     public string Q5InCorrect = "q5 inCorrect";
 
     // Question 6
+    [Header("Question 6")]
     public string Q6 = "Is it necessary to render assistance to anyone?";
     public string Q6Response1 = "q6 response 1";
     public string Q6Response2 = "q6 response 2";
@@ -61,6 +67,7 @@ public class QAFunctions: MonoBehaviour
     public string Q6InCorrect = "q6 inCorrect";
 
     // Question 7
+    [Header("Question 7")]
     public string Q7 = "Should the work group supervisor be contacted?";
     public string Q7Response1 = "q7 response 1";
     public string Q7Response2 = "q7 response 2";
@@ -69,6 +76,7 @@ public class QAFunctions: MonoBehaviour
     public string Q7InCorrect = "q7 inCorrect";
 
     // Question 8
+    [Header("Question 8")]
     public string Q8 = "Has the control measure been implemented to prevent escalation of the situation?";
     public string Q8Response1 = "q8 response 1";
     public string Q8Response2 = "q8 response 2";
@@ -77,6 +85,7 @@ public class QAFunctions: MonoBehaviour
     public string Q8InCorrect = "q8 inCorrect";
 
     // Question 9
+    [Header("Question 9")]
     public string Q9 = "Has the site been left unaltered?";
     public string Q9Response1 = "q9 response 1";
     public string Q9Response2 = "q9 response 2";
@@ -85,6 +94,7 @@ public class QAFunctions: MonoBehaviour
     public string Q9InCorrect = "q9 inCorrect";
 
     // Question 10
+    [Header("Question 10")]
     public string Q10 = "Has the work group manager been contacted to release the site?";
     public string Q10Response1 = "q10 response 1";
     public string Q10Response2 = "q10 response 2";
@@ -93,6 +103,7 @@ public class QAFunctions: MonoBehaviour
     public string Q10InCorrect = "q10 inCorrect";
 
     // Question 11
+    [Header("Question 11")]
     public string Q11 = "Have photos and witness recollections been recorded?";
     public string Q11Response1 = "q11 response 1";
     public string Q11Response2 = "q11 response 2";
