308b6d1606e40cedb137caee430af231c479c011
MarkKTH <62837294+MarkKTH@users.noreply.github.com>
2021-08-15 15:20:39 +1000

    Added new scene with new level design

--- Frozen Hourglass/Assets/Main Project/Scripts/IPadMechanics.cs
+++ Frozen Hourglass/Assets/Main Project/Scripts/IPadMechanics.cs
@@ -13,6 +13,14 @@ public class IPadMechanics : MonoBehaviour
 
     // Allow the Ipad to simulate photo taking functions
 
+    // How to take a picture with the camera - 
+    // Idea 1: Figure out how to turn the current image on the camera into a saved images or sprite
+    // Idea 2: Freeze the camera and duplicate the camera still frozen and save it, then unfreeze the camera
+
+    // Using the render texture from the camera, create a texture of the render to make an image
+
+    // This is the best tutorial on how to make a camera to take photos: https://www.youtube.com/watch?v=d-56p770t0U
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +30,9 @@ public class IPadMechanics : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Texture2D image = new Texture2D(200, 200, TextureFormat.RGB24, false);
+        GetComponent<Camera>().Render();
+        RenderTexture.active = GetComponent<Camera>().targetTexture;
+        image.ReadPixels(new Rect(0, 0, 200, 200), 0, 0);
     }
 }
