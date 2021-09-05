92f01738574abee4a6d92202ef36e1f9d5685205
MarkKTH <62837294+MarkKTH@users.noreply.github.com>
2021-09-05 21:59:51 +1000

    Finished first iteration of iPad development

--- Frozen Hourglass/Assets/Main Project/Scripts/IPadMechanics.cs
+++ Frozen Hourglass/Assets/Main Project/Scripts/IPadMechanics.cs
@@ -4,32 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class IPadMechanics : MonoBehaviour
-{
-    // Tasks for creating the IPad are as follows:
-    // Allow the IPad to simulate calling functions - 
-    // A way to get the Ipad to look like they can call a person is to create an interface that shows some kind of button that players can press to change 
-    // what is seen to a screen that can call a person
-
-
-    // Allow the Ipad to simulate photo taking functions
-
-    // How to take a picture with the camera - 
-    // Idea 1: Figure out how to turn the current image on the camera into a saved images or sprite
-    // Idea 2: Freeze the camera and duplicate the camera still frozen and save it, then unfreeze the camera
-
-    // Using the render texture from the camera, create a texture of the render to make an image
-
-    // This is the best tutorial on how to make a camera to take photos: https://www.youtube.com/watch?v=d-56p770t0U
-    //Texture2D image = new Texture2D(200, 200, TextureFormat.RGB24, false);
-    //GetComponent<Camera>().Render();
-    //RenderTexture.active = GetComponent<Camera>().targetTexture;
-    //image.ReadPixels(new Rect(0, 0, 200, 200), 0, 0);
-    
+{  
     public GameObject camera;
 
     public Material flash;
+    public Material cameraImage;
 
-    //public Button takePicture;
+    private float timer = 0;
+    private float time = 0.5f;
     
     // Start is called before the first frame update
     void Start()
@@ -50,14 +32,19 @@ public class IPadMechanics : MonoBehaviour
         // that looks like the call was answered, and a sound effect plays, sounding like someone mumbling.
     }
 
+    // Bind to the take picture button to activte
     public void pictureSnap()
-    {
-        // Instead of trying to do something hard like take a photo which we probably won't even use
-        // I will instead freeze the camera for a second, and have a sound effect play to give the illusion 
-        // of taking a picture instead, alterntively I can make it go white instead of a freeze.
-        // Make timer
+    {        
+        // Timer for flash
+        timer += Time.deltaTime;
 
-        camera.GetComponent<Image>().material = flash;
-
+        if (time <= timer && time + 0.5 >= timer)
+        {
+            camera.GetComponent<Image>().material = flash;
+        }
+        else
+        {
+            camera.GetComponent<Image>().material = cameraImage;
+        }
     }
 }
