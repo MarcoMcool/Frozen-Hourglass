da862a454d5577b8c8174231d69f380c9a081512
smarkovics <smarkovics00@gmail.com>
2021-09-21 12:23:38 +1000

    major fixes to IPad.

--- Frozen Hourglass/Assets/Main Project/Scripts/IPadMechanics.cs
+++ Frozen Hourglass/Assets/Main Project/Scripts/IPadMechanics.cs
@@ -12,6 +12,10 @@ public class IPadMechanics : MonoBehaviour
 
     private float timer = 0;
     private float time = 0.5f;
+
+    public GameObject callingScreen;
+    public GameObject callScreen;
+    public GameController gameController;
     
     // Start is called before the first frame update
     void Start()
@@ -25,12 +29,32 @@ public class IPadMechanics : MonoBehaviour
         pictureSnap();
     }
 
-    public void calling()
+    public void CallingWorkGroup()
     {
+        if (gameController.stepsCount == 11)
+        {
+            callingScreen.SetActive(true);
+            callScreen.SetActive(false);
+            StartCoroutine(WaitTimer());
+
+            gameController.stepsCount++;
+        }
         // When the user tries to call a person, being when they get click to call a person,
         // they will see a calling screen for about four seconds which will then go to another screen
         // that looks like the call was answered, and a sound effect plays, sounding like someone mumbling.
 
+    }
+
+    public void CallingSiteSupervisor()
+    {
+        if (gameController.stepsCount == 15)
+        {
+            callingScreen.SetActive(true);
+            callScreen.SetActive(false);
+            StartCoroutine(WaitTimer());
+
+            gameController.stepsCount++;
+        }
     }
 
     // Bind to the take picture button to activte
@@ -47,5 +71,16 @@ public class IPadMechanics : MonoBehaviour
         {
             camera.GetComponent<Image>().material = cameraImage;
         }
+
+        if (gameController.stepsCount == 17)
+        {
+            gameController.stepsCount++;
+        }
+    }
+
+
+    IEnumerator WaitTimer()
+    {
+        yield return new WaitForSeconds(3f);
     }
 }
