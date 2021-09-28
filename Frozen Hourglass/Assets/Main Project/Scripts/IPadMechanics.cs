5e0033b1e6dfabe5ee39dc95e1f9828597924cad
MarcoMcool <mattym12@me.com>
2021-09-28 14:39:59 +1000

    iPad code fix

--- Frozen Hourglass/Assets/Main Project/Scripts/IPadMechanics.cs
+++ Frozen Hourglass/Assets/Main Project/Scripts/IPadMechanics.cs
@@ -34,13 +34,13 @@ public class IPadMechanics : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print(Vector3.Distance(gameObject.transform.position, ladder.transform.position));
+        //print(Vector3.Distance(gameObject.transform.position, ladder.transform.position));
         // Timer for flash
         flashTimer += Time.deltaTime;
         // Timer for call to start
         callTimer += Time.deltaTime;
 
-        print("Flash Time: " + flashTimer);
+        //print("Flash Time: " + flashTimer);
 
         // Change material to white then change it back to the camera
         if (flashTime >= flashTimer)
@@ -52,35 +52,57 @@ public class IPadMechanics : MonoBehaviour
             camera.GetComponent<Image>().material = cameraImage;
         }
 
-        // Change text when after X seconds
-        if (callTime <= callTimer)
-        {
-            // Could try to increment
-            callTimeText.text = "Talking";
-            print("CAll Answered");
-            callAnswered = true;
-        }
-
-        if ((callingWorkSupervisor == true || callingSiteSupervisor == true) && callAnswered == true)
-        {
-            print("Supervisor called");
-            gameController.stepsCount++;
-            callAnswered = false;
-            callingWorkSupervisor = false;
-            callingSiteSupervisor = false;
-            if(callingWorkSupervisor == true)
-            {
-                calledWorkSupervisor = true;
-            }
-            if(callingSiteSupervisor == true)
-            {
-                calledSiteSupervisor = true;
-            }
-           
-        }
     }
 
-    public void workSupervisorCalling()
+    public void CheckCalling(int num)
+    {
+        if (!calledWorkSupervisor && num ==1)
+        {
+            callText.text = "Calling Work Group Supervisor";
+
+            calledWorkSupervisor = true;
+        }
+        else if (!calledSiteSupervisor && calledWorkSupervisor && num==2)
+        {
+            callText.text = "Calling Site Supervisor";
+
+            calledSiteSupervisor = true;
+        }
+        else
+        {
+            // Turn this into a prompt
+            print("Wrong Supervisor");
+            return;
+        }
+        StartCoroutine(Call());
+    }
+    IEnumerator Call()
+    {
+        //Print the time of when the function is first called.
+        print("Started Coroutine at timestamp : " + Time.time);
+        callingScreen.SetActive(true);
+        callScreen.SetActive(false);
+
+        callTimeText.text = "Calling...";
+
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+
+        yield return new WaitForSeconds(9);
+
+        callTimeText.text = "Talking";
+        print("CAll Answered");
+        gameController.stepsCount++;
+        yield return new WaitForSeconds(4);
+        print("Supervisor called");
+        callingScreen.SetActive(false);
+        callScreen.SetActive(true);
+
+        //After we have waited 9 seconds print the time again.
+        print("Finished Coroutine at timestamp : " + Time.time);
+    }
+
+    public void workSupervisorCalling(int num)
     {
         if (calledWorkSupervisor == false)
         {
@@ -91,7 +113,7 @@ public class IPadMechanics : MonoBehaviour
             callTimer = 0;
             callAnswered = false;
             callTimeText.text = "Calling...";
-            callingWorkSupervisor = true;
+            calledWorkSupervisor = true;
         }
         else
         {
@@ -111,7 +133,7 @@ public class IPadMechanics : MonoBehaviour
             callTimer = 0;
             callAnswered = false;
             callTimeText.text = "Calling...";
-            callingSiteSupervisor = true;
+            calledSiteSupervisor = true;
         }
         else
         {
