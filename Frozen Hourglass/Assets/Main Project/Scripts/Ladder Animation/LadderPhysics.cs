5228efddd5a8cde95c3ba054c7ce040b4b35dcdc
smarkovics <smarkovics00@gmail.com>
2021-10-03 20:46:22 +1000

    ladder falls now with a time

--- Frozen Hourglass/Assets/Main Project/Scripts/Ladder Animation/LadderPhysics.cs
+++ Frozen Hourglass/Assets/Main Project/Scripts/Ladder Animation/LadderPhysics.cs
@@ -13,13 +13,13 @@ public class LadderPhysics : MonoBehaviour
     public PhysicMaterial pmNoSlide;
     public MeshCollider meshCollider;
     public bool done;
-    // Start is called before the first frame update
+    bool ladderfall = false;
+
     void Start()
     {
         meshCollider = GetComponent<MeshCollider>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         //if (Input.GetKeyDown(KeyCode.P))
@@ -34,22 +34,33 @@ public class LadderPhysics : MonoBehaviour
     }
     public void StartFall()
     {
-
-        done = false;
-        rb.AddExplosionForce(650f, topExplosivePosition.transform.position, 10f);
-
-        StartCoroutine(Slide());
-
-        StartCoroutine(ResetSlide());
+        StartCoroutine(WaitTime());
         
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (ladderfall)
+        {
+            Ladder();
+            StartCoroutine(Slide());
+
+            StartCoroutine(ResetSlide());
+        }
+    }
+
+    void Ladder()
+    {
+        done = false;
+        rb.AddExplosionForce(500f, topExplosivePosition.transform.position, 10f);
+        ladderfall = false;
     }
 
     IEnumerator Slide()
     {
         yield return new WaitForSeconds(0.03f);
         meshCollider.material = pmSlide;
-        //pmNoSlide.dynamicFriction = 0;
-        //pmNoSlide.staticFriction = 0;
     }
 
     IEnumerator ResetSlide()
@@ -59,7 +70,11 @@ public class LadderPhysics : MonoBehaviour
         meshCollider.material = pmNoSlide;
         done = true;
         print("DONE");
-        //pmSlide.dynamicFriction = 0.3f;
-        //pmSlide.staticFriction = 0.3f;
+    }
+
+    IEnumerator WaitTime()
+    {
+        yield return new WaitForSeconds(1.5f);
+        ladderfall = true;
     }
 }
